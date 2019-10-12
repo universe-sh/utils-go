ff18f67603d6052385b61f9aafe6dc70384926ba
Aurelien Perrier <aperrier@universe.sh>
2019-10-13 00:49:54 +0200

    Adding checkenv func

--- system/system.go
+++ system/system.go
@@ -1,8 +1,20 @@
 package system
 
 import (
+	"fmt"
 	"os"
 )
+
+// CheckEnv variables
+func CheckEnv(envs []string) error {
+	for _, env := range envs {
+		if os.Getenv(env) == "" {
+			return fmt.Errorf("Env variable %s is empty or unknown", env)
+		}
+	}
+
+	return nil
+}
 
 // GetEnv variable
 func GetEnv(name, defValue string) string {
